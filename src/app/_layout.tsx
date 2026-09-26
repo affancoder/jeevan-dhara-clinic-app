a56a2d4bed3cf89c2b56d40e197af322381995c6
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2026-09-26 16:54:33 +0530

    update status bar & gesture area

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,119 +1,136 @@
 import { Stack } from "expo-router";
 import { View } from "react-native";
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+} from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      {/* Splash / Initial Screen */}
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: false,
-          headerShadowVisible: false,
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          backgroundColor: "#FFFFFF",
         }}
-      />
+        edges={["top", "bottom"]}
+      >
+        <Stack>
+          {/* Splash / Initial Screen */}
+          <Stack.Screen
+            name="index"
+            options={{
+              headerShown: false,
+              headerShadowVisible: false,
+            }}
+          />
 
-      {/* Login */}
-      <Stack.Screen
-        name="login"
-        options={{
-          headerShown: true,
-          headerTitle: "",
-          headerBackTitle: "",
-          headerShadowVisible: false,
-        }}
-      />
+          {/* Login */}
+          <Stack.Screen
+            name="login"
+            options={{
+              headerShown: true,
+              headerTitle: "",
+              headerBackTitle: "",
+              headerShadowVisible: false,
+            }}
+          />
 
-      {/* Sign Up */}
-      <Stack.Screen
-        name="sign"
-        options={{
-          headerShown: true,
-          headerTitle: "",
-          headerBackTitle: "",
-          headerShadowVisible: false,
-        }}
-      />
+          {/* Sign Up */}
+          <Stack.Screen
+            name="sign"
+            options={{
+              headerShown: true,
+              headerTitle: "",
+              headerBackTitle: "",
+              headerShadowVisible: false,
+            }}
+          />
 
-      {/* Forgot Password */}
-      <Stack.Screen
-        name="forget"
-        options={{
-          headerShown: true,
-          headerTitle: "",
-          headerBackTitle: "",
-          headerShadowVisible: false,
-        }}
-      />
+          {/* Forgot Password */}
+          <Stack.Screen
+            name="forget"
+            options={{
+              headerShown: true,
+              headerTitle: "",
+              headerBackTitle: "",
+              headerShadowVisible: false,
+            }}
+          />
 
-      <Stack.Screen
-        name="home"
-        options={{
-          headerShown: true,
-          headerBackVisible: false,
-          headerShadowVisible: false,
-          headerTitle: "",
-          header: () => (
-            <View
-              style={{
-                height: 40,
-                backgroundColor: "#FFFFFF",
-              }}
-            />
-          ),
-        }}
-      />
+          {/* Home */}
+          <Stack.Screen
+            name="home"
+            options={{
+              headerShown: true,
+              headerBackVisible: false,
+              headerShadowVisible: false,
+              headerTitle: "",
+              header: () => (
+                <View
+                  style={{
+                    height: 40,
+                    backgroundColor: "#FFFFFF",
+                  }}
+                />
+              ),
+            }}
+          />
 
-      {/* appointments */}
-      <Stack.Screen
-        name="MyAppointments"
-        options={{
-          headerShown: true,
-          headerTitle: "Booked Appointments",
-          headerTitleAlign: "center",
-        }}
-      />
+          {/* Appointments */}
+          <Stack.Screen
+            name="MyAppointments"
+            options={{
+              headerShown: true,
+              headerTitle: "Booked Appointments",
+              headerTitleAlign: "center",
+            }}
+          />
 
-      {/* Notifications */}
-      <Stack.Screen
-        name="notifications"
-        options={{
-          headerShown: true,
-          headerTitle: "Notifications",
-          headerTitleAlign: "center",
-        }}
-      />
+          {/* Notifications */}
+          <Stack.Screen
+            name="notifications"
+            options={{
+              headerShown: true,
+              headerTitle: "Notifications",
+              headerTitleAlign: "center",
+            }}
+          />
 
-      <Stack.Screen
-        name="profile"
-        options={{
-          headerShown: true,
-          headerTitle: "Profile",
-          headerTitleAlign: "center",
-        }}
-      />
+          {/* Profile */}
+          <Stack.Screen
+            name="profile"
+            options={{
+              headerShown: true,
+              headerTitle: "Profile",
+              headerTitleAlign: "center",
+            }}
+          />
 
-      {/* Doctor Categories */}
-      <Stack.Screen
-        name="DoctorCategories"
-        options={{
-          headerShown: true,
-          headerTitle: "Doctor Categories",
-          headerTitleAlign: "center",
-          headerShadowVisible: false,
-          headerSearchBarOptions: {
-            headerIconColor: "#000",
-          },
-        }}
-      />
-      {/* DoctorProfile */}
-      <Stack.Screen
-        name="DoctorProfile"
-        options={{
-          headerShown: false,
-          headerTitle: "",
-        }}
-      />
-    </Stack>
+          {/* Doctor Categories */}
+          <Stack.Screen
+            name="DoctorCategories"
+            options={{
+              headerShown: true,
+              headerTitle: "Doctor Categories",
+              headerTitleAlign: "center",
+              headerShadowVisible: false,
+              headerSearchBarOptions: {
+                headerIconColor: "#000",
+              },
+            }}
+          />
+
+          {/* Doctor Profile */}
+          <Stack.Screen
+            name="DoctorProfile"
+            options={{
+              headerShown: false,
+              headerTitle: "",
+            }}
+          />
+        </Stack>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
